6b236adb51d07da17973d6349f4d87106efa2d6d
hofmant3 <tobias.hofmann.th1@roche.com>
2023-10-05 10:06:23 +0200

    icons instead of labels; quadratic layout to support mobile better

--- duplo.py
+++ duplo.py
@@ -7,6 +7,8 @@ from copy import copy
 st.set_page_config(layout="wide")
 st.title("Duplo Eisenbahn Designer")
 
+pos0 = [(-.5,0),(.5,0)]
+
 l0 = 4
 straight = [(0,0),(1,0),(1,l0),(0,l0)]
 
@@ -77,33 +79,33 @@ def add_straight(cur_pos):
 
 if 'patches' not in st.session_state:
   st.session_state.patches = []
-  st.session_state.pos0 = [[(0,.5),(0,-.5)]]
+  st.session_state.pos0 = [pos0]
 
 with st.container():
   col1, col2, col3, col4 = st.columns(4)
   with col1:
-    if st.button('Straight'):
+    if st.button(':arrow_up:'):
       pos0 = st.session_state.pos0[-1]
       patch, pos0 = add_straight(pos0)
       st.session_state.pos0.append(pos0)
       st.session_state.patches.append(patch)
     
   with col2:
-    if st.button('Left'):
+    if st.button(':arrow_left:'):
       pos0 = st.session_state.pos0[-1]
       patch, pos0 = add_curve_left(pos0)
       st.session_state.pos0.append(pos0)
       st.session_state.patches.append(patch)
       
   with col3:
-    if st.button('Right'):
+    if st.button(':arrow_right:'):
       pos0 = st.session_state.pos0[-1]
       patch, pos0 = add_curve_right(pos0)
       st.session_state.pos0.append(pos0)
       st.session_state.patches.append(patch)
   
   with col4:
-    if st.button('Remove'):
+    if st.button(':x:'):
       st.session_state.pos0.pop()
       st.session_state.patches.pop()
     
@@ -121,8 +123,8 @@ x3 = (x1+x2)/2 + (y1-y2)
 y3 = (y1+y2)/2 - (x1-x2)
 ax.plot([x1,x2,x3,x1],[y1,y2,y3,y1],'-g',linewidth=1)
 
-ax.set_xlim((-20,60))
-ax.set_ylim((-20,20))
+ax.set_xlim((-30,30))
+ax.set_ylim((-30,30))
 plt.gca().set_aspect('equal')
 plt.tick_params(left = False, right = False , labelleft = False , labelbottom = False, bottom = False) 
 st.pyplot(fig)
