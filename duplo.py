5be1c1b879b54af9633979a880bc674a6d9eb58c
hofmant3 <tobias.hofmann.th1@roche.com>
2023-10-05 09:36:05 +0200

    with requirements

--- duplo.py
+++ duplo.py
@@ -2,6 +2,10 @@ import streamlit as st
 import matplotlib.pyplot as plt
 from matplotlib.patches import Wedge, Polygon
 import numpy as np
+from copy import copy
+
+st.set_page_config(layout="wide")
+st.title("Duplo Eisenbahn Designer")
 
 l0 = 4
 straight = [(0,0),(1,0),(1,l0),(0,l0)]
@@ -14,8 +18,6 @@ c0 = l0 * 7 / 2 / 3 ** .5
 curve_left  = [(c0-.5,0),(c0+.5,0),((c0+.5)*angc,(c0+.5)*angs),((c0-.5)*angc,(c0-.5)*angs),(0,0)]
 x = [(-x,y) for (x,y) in curve_left]
 curve_right = [x[1],x[0],x[3],x[2],x[4]]
-
-patches = []
 
 def affine_trafo(p1,p2,p1n,p2n):
   p1 = np.array(p1)
@@ -44,16 +46,6 @@ def angle_between_vectors(v1,v2):
 def angle_against_horizontal(v1):
   v2 = np.array((1,0))
   return angle_between_vectors(v1,v2)
-  
-def add_straight(ax,cur_pos):
-  
-  trafo = affine_trafo(straight[0],straight[1],cur_pos[0],cur_pos[1])
-  
-  st0 = [trafo(p).tolist()[0] for p in straight]
-  
-  ax.add_patch(Polygon(st0,edgecolor='k',fill=False)) 
-  new_pos = (st0[3],st0[2])
-  return ax, new_pos
 
 def cv(curve, cur_pos):
   trafo = affine_trafo(curve[0],curve[1],cur_pos[0],cur_pos[1])
@@ -66,44 +58,58 @@ def cv(curve, cur_pos):
   new_pos = (cu0[3],cu0[2])
   return center, r_outer, r_inner, theta1, theta2, new_pos
 
-def add_curve_left(ax,cur_pos):
+def add_curve_left(cur_pos):
   center, r_outer, r_inner, theta1, theta2, new_pos = cv(curve_left, cur_pos)
-  ax.add_patch(Wedge(center,r_outer,theta1,theta2,r_inner,edgecolor='k',fill=False))
-  return ax, new_pos
+  patch = Wedge(center,r_outer,theta1,theta2,r_inner,edgecolor='k',fill=False)
+  return patch, new_pos
 
-def add_curve_right(ax,cur_pos):
+def add_curve_right(cur_pos):
   center, r_outer, r_inner, theta1, theta2, new_pos = cv(curve_right, cur_pos)  
-  ax.add_patch(Wedge(center,r_outer,theta2,theta1,r_inner,edgecolor='k',fill=False)) 
-  return ax, new_pos
+  patch = Wedge(center,r_outer,theta2,theta1,r_inner,edgecolor='k',fill=False)
+  return patch, new_pos
 
-if 'ax' not in st.session_state:
-    fig, ax = plt.subplots()
-    st.session_state.ax = ax
-    st.session_state.fig = fig
-    st.session_state.pos0 = [(0,0),(1,0)]
+def add_straight(cur_pos):  
+  trafo = affine_trafo(straight[0],straight[1],cur_pos[0],cur_pos[1])  
+  st0 = [trafo(p).tolist()[0] for p in straight]  
+  patch = Polygon(st0,edgecolor='k',fill=False)
+  new_pos = (st0[3],st0[2])
+  return patch, new_pos
 
-if st.button('Straight'):
-  ax = st.session_state.ax
-  pos0 = st.session_state.pos0 
-  ax, pos0  = add_straight(ax, pos0)
-  st.session_state.ax = ax
-  st.session_state.pos0 = pos0
+if 'patches' not in st.session_state:
+  st.session_state.patches = []
+  st.session_state.pos0 = [[(0,.5),(0,-.5)]]
+
+if st.sidebar.button('Straight'):
+  pos0 = st.session_state.pos0[-1]
+  patch, pos0 = add_straight(pos0)
+  st.session_state.pos0.append(pos0)
+  st.session_state.patches.append(patch)
   
-if st.button('Left'):
-  ax = st.session_state.ax
-  pos0 = st.session_state.pos0 
-  ax, pos0  = add_curve_left(ax, pos0)
-  st.session_state.ax = ax
-  st.session_state.pos0 = pos0
+if st.sidebar.button('Left'):
+  pos0 = st.session_state.pos0[-1]
+  patch, pos0 = add_curve_left(pos0)
+  st.session_state.pos0.append(pos0)
+  st.session_state.patches.append(patch)
   
-if st.button('Right'):
-  ax = st.session_state.ax
-  pos0 = st.session_state.pos0 
-  ax, pos0  = add_curve_right(ax, pos0)
-  st.session_state.ax = ax
-  st.session_state.pos0 = pos0
-
-st.session_state.ax.set_xlim((-60,60))
-st.session_state.ax.set_ylim((-60,60))
-plt.axis('off')
-st.session_state.fig
+if st.sidebar.button('Right'):
+  pos0 = st.session_state.pos0[-1]
+  patch, pos0 = add_curve_right(pos0)
+  st.session_state.pos0.append(pos0)
+  st.session_state.patches.append(patch)
+  
+if st.sidebar.button('Remove'):
+  st.session_state.pos0.pop()
+  st.session_state.patches.pop()
+  
+plt.cla()
+fig, ax = plt.subplots()
+for p in st.session_state.patches:
+  p2 = copy(p)
+  ax.add_patch(p2)
+pos0 = st.session_state.pos0[-1]
+#ax.plot(pos0,'-r')
+ax.set_xlim((-20,60))
+ax.set_ylim((-20,20))
+plt.gca().set_aspect('equal')
+plt.tick_params(left = False, right = False , labelleft = False , labelbottom = False, bottom = False) 
+st.pyplot(fig)
