8bfcbb71e5f812e90a5c2f3ca92124341e31ef87
Tobias Hofmann (rm2mw061244) <tobias.hofmann.th1@roche.com>
2023-10-10 17:18:51 +0200

    doesnt work

--- test_dash.py
+++ test_dash.py
@@ -6,8 +6,6 @@ from numpy import pi, sin, cos
 import plotly.graph_objects as go
 
 
-
-
 pos0 = [[(9.5,10),(10.5,10)]]
 
 l0 = 4
@@ -21,6 +19,12 @@ c0 = l0 * 7 / 2 / 3 ** .5
 curve_left  = [(c0-.5,0),(c0+.5,0),((c0+.5)*angc,(c0+.5)*angs),((c0-.5)*angc,(c0-.5)*angs),(0,0)]
 x = [(-x,y) for (x,y) in curve_left]
 curve_right = [x[1],x[0],x[3],x[2],x[4]]
+
+def straight(st):
+    path = f"M {st[0]},{st[0]}"
+    for x,y in st:
+        path += f" L{x},{y}"
+    return path + " Z" 
 
 def wedge(center, ri, ro, th0, th1, n=50):
     t = np.linspace(th0, th1, n)
@@ -89,7 +93,7 @@ def add_curve_right(cur_pos):
 def add_straight(cur_pos):  
   trafo = affine_trafo(straight[0],straight[1],cur_pos[0],cur_pos[1])  
   st0 = [trafo(p).tolist()[0] for p in straight]  
-  patch = Polygon(st0,edgecolor='k',fill=False)
+  patch = straight(st0)
   new_pos = (st0[3],st0[2])
   return patch, new_pos
 
@@ -104,21 +108,28 @@ def add_straight(cur_pos):
 
 
 
-app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
+app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP]#,
+    #            meta_tags=[
+    #    {"name": "viewport", "content": "width=device-width, initial-scale=1"}
+    #]
+    )
 
 title = dash.html.H1("Duplo Schienen Designer")
 
 controls = dbc.Row(
             [
                 dbc.Col(dash.html.Button('\U000021B0',id="add_left")),
-                dbc.Col(dash.html.Button('\U00002191')),
-                dbc.Col(dash.html.Button('\U000021B1')),
-                dbc.Col(dash.html.Button('\U0000274C'))
+                dbc.Col(dash.html.Button('\U00002191',id="add_straight")),
+                dbc.Col(dash.html.Button('\U000021B1',id="add_right")),
+                dbc.Col(dash.html.Button('\U0000274C',id="remove"))
             ]
         )
 
 shapes0 = []
 
+
+print("hi")
+    
 @dash.callback(
     dash.Output('mygraph', 'figure'),
     dash.Input('add_left', 'n_clicks')
@@ -126,22 +137,57 @@ shapes0 = []
 def update(input_value): 
     global shapes0, pos0
     
-    if input_value > 0:
-        wedge0, new_pos = add_curve_left(pos0[-1])
-        
-        pos0 += [new_pos]
-        
-        new_shape = dict(type="path", path=wedge0)
-        
-        shapes0 += [new_shape]
+    print("hi")
+    
+#    if input_value > 0:
+    wedge0, new_pos = add_curve_left(pos0[-1])
+    
+    pos0 += [new_pos]
+    
+    new_shape = dict(type="path", path=wedge0, line={"color":"black"})
+    
+    shapes0 += [new_shape]
     
     fig0 = go.Figure()
     fig0.update_layout(template="none",
                        xaxis=axis_dict,
                        yaxis=axis_dict,
-                       xaxis_range=[0,20], yaxis_range=[0,20],
+                       xaxis_range=[-20,20], yaxis_range=[-20,20],
                        shapes = shapes0)
+    
+  #  fig0.layout._config= config
+
     return fig0
+
+
+# @dash.callback(
+#     dash.Output('mygraph', 'figure'),
+#     dash.Input('add_straight', 'n_clicks')
+# )
+# def update2(input_value): 
+#     global shapes0, pos0
+    
+#     if input_value > 0:
+#         wedge0, new_pos = add_straight(pos0[-1])
+        
+#         pos0 += [new_pos]
+        
+#         new_shape = dict(type="path", path=wedge0)
+        
+#         shapes0 += [new_shape]
+    
+#     fig0 = go.Figure()
+#     fig0.update_layout(template="none",
+#                        xaxis=axis_dict,
+#                        yaxis=axis_dict,
+#                        xaxis_range=[0,20], yaxis_range=[0,20],
+#                        shapes = shapes0)
+    
+# #    fig0.layout._config= config
+
+#     return fig0
+
+#config = {'responsive': True}
 
 axis_dict =dict(
     #visible = False,
@@ -154,16 +200,22 @@ axis_dict =dict(
                 showline=True
                 )
 
+axis_dict = dict()
+
 fig0 = go.Figure()
 fig0.update_layout(template="none",
                    xaxis=axis_dict,
                    yaxis=axis_dict,
-                   xaxis_range=[0,10], yaxis_range=[0,10])
+                   xaxis_range=[-20,20], yaxis_range=[-20,20],
+                   shapes = shapes0)
     
+#fig0.layout._config= config
+
 plot = dash.dcc.Graph(
       id='mygraph',
-      figure = fig0,
-      style={'width': '90vh', 'height': '90vh'}
+      figure = fig0#,
+      #responsive  = True,
+      #style={'width': '90vw', 'height': '90vh'}
   )
 
 app.layout = dbc.Container(
