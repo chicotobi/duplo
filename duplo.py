f963fddf7366306347b39238e37715f3693918eb
Tobias Hofmann (rm2mw061244) <tobias.hofmann.th1@roche.com>
2023-10-16 09:33:36 +0200

    fix some whitespace

--- duplo.py
+++ duplo.py
@@ -1,43 +1,31 @@
-from geometry import add_curve_left, add_curve_right, add_straight, get_front_arrow
+from geometry import add_curve_left, add_curve_right, add_straight, get_front_arrow, pos0
 from dash import Dash, html, ctx, callback, Input, Output, dcc
 
 from dash_bootstrap_components import Row, Col, Container, themes
 import plotly.graph_objects as go
 
 # State variables
-pos0 = [(-0.5,0),(0.5,0)]
 tracks = []
 
-app = Dash(external_stylesheets=[themes.BOOTSTRAP],
-                meta_tags=[ {"name": "viewport", "content": "width=device-width, initial-scale=1"} ]
-    )
-
-title = html.H1("Duplo Schienen Designer")
-
-Output('refresh_button', 'children')
-
-arrow_left     = "\U000021B0"
-arrow_straight = "\U00002191"
-arrow_right    = "\U000021B1"
-cross_remove   = "\U0000274C"
-bomb_reset     = "\U0001F4A3"
-
 def get_label_arrow_left(tracks):
+  arrow_left = "\U000021B0"
   return arrow_left + "x" + str(sum(1 for i in tracks if i.ttype == 'curve'))
-  
+
 def get_label_arrow_right(tracks):
+  arrow_right = "\U000021B1"
   return arrow_right + "x" + str(sum(1 for i in tracks if i.ttype == 'curve'))
-  
+
 def get_label_arrow_straight(tracks):
+  arrow_straight = "\U00002191"
   return arrow_straight + "x" + str(sum(1 for i in tracks if i.ttype == 'straight'))
 
-controls = Row([
-                Col(html.Button(get_label_arrow_left(tracks),id="add_left")),
-                Col(html.Button(get_label_arrow_straight(tracks),id="add_straight")),
-                Col(html.Button(get_label_arrow_right(tracks),id="add_right")),
-                Col(html.Button(cross_remove,id="remove")),
-                Col(html.Button(bomb_reset,id="reset"))                
-            ],className="g-0")
+def get_label_remove():
+  cross_remove = "\U0000274C"
+  return cross_remove
+
+def get_label_reset():
+  bomb_reset = "\U0001F4A3"
+  return bomb_reset
 
 def create_figure(shapes=[]):
     axis_dict = dict(showgrid=False, showticklabels=False, visible = False, ticks="")
@@ -47,10 +35,10 @@ def create_figure(shapes=[]):
                         yaxis=axis_dict,
                         xaxis_range=[-20,20], yaxis_range=[-20,20],
                         margin = go.layout.Margin(l = 0, r = 0, b = 0, t = 0),
-                        shapes = shapes)    
+                        shapes = shapes)
     fig0.update_yaxes(scaleanchor="x", scaleratio=1)
     return fig0
-    
+
 @callback(
     Output('mygraph', 'figure'),
     Output('add_left', 'children'),
@@ -63,14 +51,14 @@ def create_figure(shapes=[]):
     Input('reset', 'n_clicks'),
     prevent_initial_call=True
 )
-def update(b1, b2, b3, b4, b5): 
+def update(b1, b2, b3, b4, b5):
     global tracks
-    
+
     if len(tracks) > 0:
         cur_pos = tracks[-1].ending
     else:
         cur_pos = pos0
-        
+
     if ctx.triggered_id == 'remove' and len(tracks):
         if len(tracks) > 0:
             tracks.pop()
@@ -80,28 +68,43 @@ def update(b1, b2, b3, b4, b5):
         if ctx.triggered_id == 'add_left':
             new_track = add_curve_left(cur_pos)
         elif ctx.triggered_id == 'add_right':
-            new_track = add_curve_right(cur_pos)    
+            new_track = add_curve_right(cur_pos)
         elif ctx.triggered_id == 'add_straight':
-            new_track = add_straight(cur_pos)  
+            new_track = add_straight(cur_pos)
         tracks.append(new_track)
-    
+
     if len(tracks) > 0:
         cur_pos = tracks[-1].ending
     else:
         cur_pos = pos0
-        
+
     front_arrow = get_front_arrow(cur_pos)
-        
-    track_shapes = [i.shape for i in tracks]
-    
-    fig = create_figure(track_shapes + [front_arrow])
+
+    track_shapes = [i.shape for i in tracks] + [front_arrow]
+
+    fig = create_figure(track_shapes )
     label_arrow_left     = get_label_arrow_left(tracks)
     label_arrow_straight = get_label_arrow_straight(tracks)
     label_arrow_right    = get_label_arrow_right(tracks)
-    
+
     return fig, label_arrow_left, label_arrow_straight, label_arrow_right
 
+
+app = Dash(external_stylesheets=[themes.BOOTSTRAP],
+                meta_tags=[ {"name": "viewport", "content": "width=device-width, initial-scale=1"} ]
+    )
+
+title = html.H1("Duplo Schienen Designer")
+
 fig0 = create_figure()
+
+controls = Row([
+                Col(html.Button(get_label_arrow_left(tracks),id="add_left")),
+                Col(html.Button(get_label_arrow_straight(tracks),id="add_straight")),
+                Col(html.Button(get_label_arrow_right(tracks),id="add_right")),
+                Col(html.Button(get_label_remove(),id="remove")),
+                Col(html.Button(get_label_reset(),id="reset"))
+            ],className="g-0")
 
 plot = dcc.Graph(id='mygraph', figure = fig0, style={'width': '90vw', 'height': '90vh'})
 plot_with_border = html.Div(plot, style={"border":"2px black solid"})
--- geometry.py
+++ geometry.py
@@ -1,6 +1,8 @@
 from track import Track
 import numpy as np
 
+pos0 = [(-0.5,0),(0.5,0)]
+
 l0 = 4
 straight = [(0,0),(1,0),(1,l0),(0,l0)]
 
@@ -18,18 +20,18 @@ def affine_trafo(p1,p2,p1n,p2n):
   p2  = np.array(p2)
   p1n = np.array(p1n)
   p2n = np.array(p2n)
-  
+
   v = p2 - p1
   vn = p2n - p1n
-  
+
   ang = angle_between_vectors(vn,v)
-  
-  # Find rotational matrix  
+
+  # Find rotational matrix
   A = np.matrix([[np.cos(ang), -np.sin(ang)], [np.sin(ang), np.cos(ang)]])
-  
+
   def tmp(pt):
     return A.dot(np.array(pt)-p1) + p1n
-  
+
   return tmp
 
 def angle_between_vectors(v1,v2):
@@ -46,7 +48,7 @@ def cv(curve, cur_pos):
   cu0 = [trafo(p).tolist()[0] for p in curve]
   r_outer = np.linalg.norm(np.array(cu0[1])-np.array(cu0[4]))
   r_inner = np.linalg.norm(np.array(cu0[0])-np.array(cu0[4]))
-  theta1 = angle_against_horizontal(np.array(cu0[0])-np.array(cu0[4])) 
+  theta1 = angle_against_horizontal(np.array(cu0[0])-np.array(cu0[4]))
   center = cu0[4]
   new_pos = (cu0[3],cu0[2])
   return center, r_outer, r_inner, theta1, new_pos
@@ -55,7 +57,7 @@ def shape_straight(st):
     path = f"M {st[0][0]},{st[0][1]}"
     for x,y in st[1:]:
         path += f" L{x},{y}"
-    path += " Z" 
+    path += " Z"
     return dict(type="path", path=path)
 
 def shape_wedge(center, ri, ro, th0, th1, n=50):
@@ -71,7 +73,7 @@ def shape_wedge(center, ri, ro, th0, th1, n=50):
     path = f"M {x[0]},{y[0]}"
     for xc, yc in zip(x[1:], y[1:]):
         path += f" L{xc},{yc}"
-    path += " Z" 
+    path += " Z"
     return dict(type="path", path=path)
 
 def add_curve_left(cur_pos):
@@ -80,13 +82,13 @@ def add_curve_left(cur_pos):
   return Track(ttype = "curve", ending = new_pos, shape = shape)
 
 def add_curve_right(cur_pos):
-  center, r_outer, r_inner, theta1, new_pos = cv(curve_right, cur_pos)  
+  center, r_outer, r_inner, theta1, new_pos = cv(curve_right, cur_pos)
   shape = shape_wedge(center ,r_inner, r_outer, theta1 - np.pi/6, theta1)
   return Track(ttype = "curve", ending = new_pos, shape = shape)
 
-def add_straight(cur_pos):  
-  trafo = affine_trafo(straight[0],straight[1],cur_pos[0],cur_pos[1])  
-  st0 = [trafo(p).tolist()[0] for p in straight]  
+def add_straight(cur_pos):
+  trafo = affine_trafo(straight[0],straight[1],cur_pos[0],cur_pos[1])
+  st0 = [trafo(p).tolist()[0] for p in straight]
   shape = shape_straight(st0)
   new_pos = (st0[3],st0[2])
   return Track(ttype = "straight", ending = new_pos, shape = shape)
