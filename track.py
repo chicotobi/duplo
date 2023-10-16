caf8677d55fa90052419f5031346fad1ce65da88
Tobias Hofmann (rm2mw061244) <tobias.hofmann.th1@roche.com>
2023-10-16 14:03:28 +0200

    remove old example code

--- track.py
+++ track.py
@@ -2,9 +2,6 @@ from dataclasses import  dataclass
 
 @dataclass
 class Track:
-    ttype: str    
+    ttype: str
     ending: None
     shape: None
-
-    def get_shape(self):        
-        return self.unit_price * self.quantity_on_hand
